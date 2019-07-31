006afb5e2bcb70cc7ba731e9257eddd6d0405208
Jonathan_Ishii <jtishii@yahoo.com>
2019-07-30 18:07:09 -0700

    Fully functioning program

--- consoleToCSV/consoleToCSV.py
+++ consoleToCSV/consoleToCSV.py
@@ -1,13 +1,14 @@
 # my program from converting safari console for Tetris_AI to a csv file format
 
-infilename = "Console_41_49.txt"
-outfilename = "Console_41_49_cleaned.txt"
+infilename = "Console_0_49.txt"
+outfilename = "Console_0_49_cleaned.txt"
 inputFile = open(infilename,"r")
 outputFile = open(outfilename,"w")
 
 for line in inputFile.readlines():
     tempStr = line.replace("[Log] ", "")
     tempStr = tempStr.replace(" (genetic-algorithm.js, line 324", "")
+    tempStr = tempStr.replace(" (genetic-algorithm.js, line 191", "")
     tempStr = tempStr.replace(")", "")
     try:
         index = tempStr.index('x')
--- start-ai.js
+++ start-ai.js
@@ -7,7 +7,7 @@ function playForMe()
   StartGame();
    // Run individual numbers to make sure that it is running properly
    // Think this one got 5000+ score
-  var best = new BestFirstSearch(-0.6510316230834794,-0.3354738073259488,-0.40389225333322976,0.41574260159720805);
+  var best = new BestFirstSearch(-0.6510316230834794,-0.3354738073259488,-0.40389225333322976,0.5754133093214668);
   best.startAI();
   // var steepest = new SteepestHillClimbing();
   // steepest.startAI();
